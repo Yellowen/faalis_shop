53a69af9d888157d21366657ba5a0336701326c3
Behnam Ahmad Khan Beigi <yottanami@gnu.org>
2016-05-16 11:14:13 +0430

    Sitebar menu items created

--- lib/faalis/shop/sidebar.rb
+++ lib/faalis/shop/sidebar.rb
@@ -1,8 +1,8 @@
 class Faalis::Dashboard::Models::Sidebar
   def shop_entries
-    menu I18n.t('faalis.shop.dashboard.shop'), icon: 'fa fa-globe' do
+    menu I18n.t('faalis.shop.dashboard.shop'), icon: 'fa fa-cart-plus' do
       item(I18n.t('faalis.shop.dashboard.products'),
-        model: 'Faalis::Shop::Products',
+        model: 'Faalis::Shop::Product',
         url: Faalis::Shop::Engine.routes.url_helpers.dashboard_products_path)
       item(I18n.t('faalis.shop.dashboard.categories'),
         model: 'Faalis::Shop::Category',
