9573468180a4e25eefffec346191fd7f3bf8d191
Sameer Rahmani <lxsameer@gnu.org>
2016-06-12 14:57:10 +0430

    layout added to product controller

--- app/controllers/faalis/shop/products_controller.rb
+++ app/controllers/faalis/shop/products_controller.rb
@@ -2,6 +2,7 @@ require_dependency "faalis/application_controller"
 
 module Faalis::Shop
   class ProductsController < ::ApplicationController
+    layout Faalis::Shop::Engine.default_layout
 
     def index
       @products = Product.find_by(user: current_user)
